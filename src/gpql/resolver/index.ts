baedb83c4c879ab5a49211e3f03e538ac8cadac6
sabbir1054 <mdsabbir1054@gmail.com>
2024-02-03 22:45:50 +0600

    category retrieve impliment

---
+++ src/gpql/resolver/index.ts
@@ -0,0 +1,16 @@
+import { db } from "../../db.js";
+
+export const resolvers = {
+  Query: {
+    products: () => db.products,
+    product: (parent: any, args: { productId: string }, context: any) => {
+      const result = db.products.find((pd) => pd.id === args.productId);
+      return result;
+    },
+    categories: () => db.categories,
+    category: (parent: any, args: { categoryId: string }, context: any) => {
+      const result = db.categories.find((pd) => pd.id === args.categoryId);
+      return result;
+    },
+  },
+};
